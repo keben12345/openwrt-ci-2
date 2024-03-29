d7f2a90d583fa91ca32445863aaf68370b2d12df
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-03-29 17:12:39 +0800

    Update ax5jdc.sh

--- ax5jdc.sh
+++ ax5jdc.sh
@@ -12,7 +12,7 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone --depth=1 https://github.com/kenzok8/small.git package/small
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*}
 rm -rf feeds/packages/utils/v2dat
 rm -rf package/small/luci-app-bypass
 
