d590fa5dde1df5cd865c17d8253576659bc7f43b
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-07-16 19:54:05 +0800

    Update ufi.sh

--- scripts/ufi.sh
+++ scripts/ufi.sh
@@ -1,2 +1,7 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-git clone --depth=1 https://github.com/xiaolu/mkbootimg_tools.git mkbootimg_tools
+
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
+rm -rf feeds/packages/net/{microsocks,xray*,v2ray*,sing*}
+rm -rf feeds/packages/utils/v2dat
