0df167d3d569f44a0f1cf7070662c3c22a0890bc
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-07-21 17:39:44 +0800

    Update ufi.sh

--- script/ufi.sh
+++ script/ufi.sh
@@ -7,3 +7,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds install -a
 rm -rf feeds/small/luci-app-bypass
 rm -rf feeds/small/luci-app-ssr-plus
+rm -rf feeds/small/luci-app-ssr-plus
+rm -rf feeds/packages/net/modemmanager
+rm -rf feeds/luci/protocols/luci-proto-modemmanager
+sed -i 's/qcom-msm8916-modem-openstick-ufi003-firmware //g' target/linux/msm89xx/image/msm8916.mk
