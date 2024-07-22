0e220485390971ee8c3ab1a29a4addfb30b37b60
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-07-22 17:08:42 +0800

    Update ufi.sh

--- script/ufi.sh
+++ script/ufi.sh
@@ -8,6 +8,7 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 # rm -rf feeds/small/luci-app-bypass
 # rm -rf feeds/small/luci-app-ssr-plus
 
+rm -rf feeds/luci/applications/luci-app-dockerman
 git clone --depth=1 https://github.com/immortalwrt/luci.git package/imm-luci
 mkdir package/mypkg
 mv package/imm-luci/applications/luci-app-dockerman package/mypkg/
