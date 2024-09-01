eb80f16e1a5f369b3a3ac5b569af2e845c0e54a1
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-09-01 14:06:04 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -1,7 +1,7 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
-# 添加kenzok8_small插件库, 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
+# 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
 rm -rf feeds/packages/lang/golang
 git clone --depth=1 https://github.com/kenzok8/golang feeds/packages/lang/golang
 #删除自带的老旧依赖，ssr-plus，passwall
@@ -19,21 +19,27 @@ git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclas
 mv package/openclash/luci-app-openclash feeds/luci/applications/
 rm -rf package/openclash
 
-#安装最新ddns
-rm -rf feeds/luci/applications/luci-app-ddns
-rm -rf feeds/packages/net/ddns-scripts
-git clone --depth=1 https://github.com/kiddin9/openwrt-packages.git  package/kiddin9
-mv package/kiddin9/luci-app-ddns feeds/luci/applications/
-mv package/kiddin9/ddns-scripts feeds/packages/net/ddns-scripts
-rm -rf package/kiddin9
-
-
+#原插件替换为immortalwrt的最新版
 mkdir package/mypkg
 git clone --depth=1 https://github.com/immortalwrt/luci.git package/imm-luci
+git clone --depth=1 https://github.com/immortalwrt/packages.git package/imm-pkg
 mv package/imm-luci/luci.mk  package/luci.mk
-
+#ddns
+rm -rf feeds/luci/applications/luci-app-ddns
+rm -rf feeds/packages/net/ddns-scripts
+mv package/imm-luci/luci-app-ddns package/mypkg/
+mv package/imm-pkg/ddns-scripts package/mypkg/
+#wireguard
 rm -rf feeds/luci/protocols/luci-proto-wireguard
 mv package/imm-luci/protocols/luci-proto-wireguard package/mypkg/
+#socat
+rm -rf feeds/luci/applications/luci-app-socat
+rm -rf feeds/packages/net/socat
+mv package/imm-luci/luci-app-ddns package/mypkg/
+mv package/imm-pkg/ddns-scripts package/mypkg/
 
-mv package/imm-luci/applications/luci-app-ramfree package/mypkg/
 rm -rf package/imm-luci
+rm -rf package/imm-pkg
+
+# git clone --depth=1 https://github.com/kiddin9/openwrt-packages.git  package/kiddin9
+# rm -rf package/kiddin9
