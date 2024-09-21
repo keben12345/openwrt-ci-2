5e15d55514ec8ee7981fc6169fb37b3910ce9f82
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-09-21 20:22:09 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -20,4 +20,4 @@ mv package/openclash/luci-app-openclash feeds/luci/applications/
 rm -rf package/openclash
 
 #下载5g模块
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
