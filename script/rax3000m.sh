6914c18ac85c85b7724f7b5f3a6f7ce6ce8b516d
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-24 19:11:21 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -1,4 +1,5 @@
-sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 
 
