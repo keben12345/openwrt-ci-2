62a09a9b47dc3b47295979b70ecd4a2ce837f97c
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-24 13:54:39 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -1,17 +1,6 @@
 sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 
-# 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本 ，可以用以下命令
-rm -rf feeds/packages/lang/golang
-git clone --depth=1 https://github.com/kenzok8/golang feeds/packages/lang/golang
-#删除自带的老旧依赖，ssr-plus，passwall
-rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,gn,hysteria,ipt2socks,microsocks,naiveproxy}
-rm -rf feeds/packages/net/{pdnsd-alt,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*}
-rm -rf feeds/packages/net/{shadowsocks-rust,shadowsocksr-libev}
-rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-plus,luci-app-vssr}
-#下载passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/paswall-app
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/paswall-pkg
 
 #安装最新openclash
 rm -rf feeds/luci/applications/luci-app-openclash
@@ -20,4 +9,4 @@ mv package/openclash/luci-app-openclash feeds/luci/applications/
 rm -rf package/openclash
 
 #下载5g模块
-# git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
