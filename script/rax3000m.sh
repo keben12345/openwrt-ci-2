214efde6ffbac49267fec136669902876467796d
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-26 20:25:22 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/RAX3000M/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/XR30/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/XR30/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 
 #安装最新openclash
