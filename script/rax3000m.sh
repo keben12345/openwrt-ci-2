b62d473f493a739796bdeac4d04898182862b3a5
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-29 16:56:10 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -1,13 +1,14 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/XR30/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/XR30/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/rax3000m64g/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/rax3000m64g/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 
 #安装最新openclash
-rm -rf feeds/luci/applications/luci-app-openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
-mv package/openclash/luci-app-openclash feeds/luci/applications/
-rm -rf package/openclash
+#rm -rf feeds/luci/applications/luci-app-openclash
+#git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
+#mv package/openclash/luci-app-openclash feeds/luci/applications/
+#rm -rf package/openclash
 
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
