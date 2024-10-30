68b6358699b0ede81daeee2c9aa601bdc06d10a2
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-30 11:17:45 +0800

    Update mt798x-23.05.sh

--- script/mt798x-23.05.sh
+++ script/mt798x-23.05.sh
@@ -11,6 +11,7 @@ mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/ban
 
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+rm -rf feeds/packages/net/quectel-cm
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
