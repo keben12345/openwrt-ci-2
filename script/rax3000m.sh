565aaeef64d4c4ed3216434ed6e77e5cd57a1a9f
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-30 11:49:30 +0800

    Update rax3000m.sh

--- script/rax3000m.sh
+++ script/rax3000m.sh
@@ -9,5 +9,14 @@ mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/ban
 #mv package/openclash/luci-app-openclash feeds/luci/applications/
 #rm -rf package/openclash
 
-#下载5g模块
+#下载
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+rm -rf package/istore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
+
+sed -i 's/mirrors.vsean.net/mirror.nju.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
+sed -i 's/openwrt/immortalwrt/g' package/emortal/default-settings/files/99-default-settings-chinese
