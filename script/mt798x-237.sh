8c3dbe7d17dbdbe1e56c6d80491ff3572225df62
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-10-30 14:22:16 +0800

    Update mt798x-237.sh

--- script/mt798x-237.sh
+++ script/mt798x-237.sh
@@ -1,9 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/rax3000m/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/rax3000m/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
-mv $GITHUB_WORKSPACE/patch/m798x-closed/defset $OPENWRT_PATH/package/emortal/default-settings/files/99-default-settings
-mv $GITHUB_WORKSPACE/patch/m798x-closed/key-build.pub $OPENWRT_PATH/package/istore/luci-app-store/luci/luci-app-store/src/key-build.pub
 
 #安装最新openclash
 #rm -rf feeds/luci/applications/luci-app-openclash
