a6e71ea7af5c8625d76af78ff677092fb06926f5
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-06 22:02:08 +0800

    Update mt798x-23.05.sh

--- script/mt798x-23.05.sh
+++ script/mt798x-23.05.sh
@@ -1,22 +1,12 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/rax3000m64g/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/rax3000m64g/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/AX6000/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/AX6000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
-mv $GITHUB_WORKSPACE/patch/m798x-23.05-closed/defset $OPENWRT_PATH/package/emortal/default-settings/files/99-default-settings
+mv $GITHUB_WORKSPACE/patch/m798x-23.05-padavanonly/defset $OPENWRT_PATH/package/emortal/default-settings/files/99-default-settings
 chmod a+rwx package/emortal/default-settings/files/99-default-settings
 sed -i 's#downloads.immortalwrt.org#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 
-#安装最新openclash
-#rm -rf feeds/luci/applications/luci-app-openclash
-#git clone --depth=1 https://github.com/vernesong/OpenClash.git  package/openclash
-#mv package/openclash/luci-app-openclash feeds/luci/applications/
-#rm -rf package/openclash
 
 #下载5g模块
-git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-rm -rf feeds/packages/net/quectel-cm
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-mv package/nas-packages/network/services/* package/nas-packages/
-rm -rf package/nas-packages/network
+#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
+#rm -rf feeds/packages/net/quectel-cm
