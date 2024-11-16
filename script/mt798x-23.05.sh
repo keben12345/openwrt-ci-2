2ed953b6a756e3e3610fdd1a76d2cc1c4cd92589
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-16 14:40:05 +0800

    Update mt798x-23.05.sh

--- script/mt798x-23.05.sh
+++ script/mt798x-23.05.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/AX3000/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/AX3000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/AX6000/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/AX6000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/m798x-23.05-padavanonly/defset $OPENWRT_PATH/package/emortal/default-settings/files/99-default-settings
 chmod a+rwx package/emortal/default-settings/files/99-default-settings
@@ -19,8 +19,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/p
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/ssr
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
-mv package/nas-packages/network/services/* package/nas-packages/
-rm -rf package/nas-packages/network
+#git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+#git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+#git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+#mv package/nas-packages/network/services/* package/nas-packages/
+#rm -rf package/nas-packages/network
