400ebbd996936214fb94dab3108caa5f6d090032
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-23 19:12:13 +0800

    Update hanwckf-mt798x.sh

--- script/hanwckf-mt798x.sh
+++ script/hanwckf-mt798x.sh
@@ -2,8 +2,8 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/ImmortalWrt/AX3000t/g' package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
-sed -i "s/ImmortalWrt/AX3000t/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+#sed -i 's/ImmortalWrt/AX3000t/g' package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+#sed -i "s/ImmortalWrt/AX3000t/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 
 #安装最新openclash
