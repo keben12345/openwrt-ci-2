d5668d168fc8cf65461a5b91849a6c9e06eff097
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-23 19:12:26 +0800

    Update mt798x-237.sh

--- script/mt798x-237.sh
+++ script/mt798x-237.sh
@@ -1,4 +1,4 @@
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/AX3000/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/AX3000/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
