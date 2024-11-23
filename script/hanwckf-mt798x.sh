761943e22f45bfafe68231894836ade3fec3f6b8
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-23 14:44:33 +0800

    Update hanwckf-mt798x.sh

--- script/hanwckf-mt798x.sh
+++ script/hanwckf-mt798x.sh
@@ -1,7 +1,7 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/ImmortalWrt/OpenWrt/g' package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ImmortalWrt/AX3000/g' package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 
 #安装最新openclash
@@ -11,8 +11,6 @@ mv package/openclash/luci-app-openclash feeds/luci/applications/
 rm -rf package/openclash
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/mypackage/istore
-# git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/mypackage/nas-packages
-# git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/mypackage/nas-luci
-# mv package/mypackage/nas-packages/network/services/* package/mypackage/nas-packages/
-# rm -rf package/mypackage/nas-packages/network
+git clone --depth=1 https://github.com/xiangfeidexiaohuo/extra-ipk.git package/extra-ipk
+mv package/extra-ipk/linkease package/linkease
+rm -rf package/extra-ipk
