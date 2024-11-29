e2ac45a28b71132d9eb3d0ec6ac24f1f026d1be4
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-11-29 15:23:55 +0800

    Update hanwckf-mt798x-science.sh

--- script/hanwckf-mt798x-science.sh
+++ script/hanwckf-mt798x-science.sh
@@ -35,8 +35,8 @@ rm -rf package/passwall-packages/shadowsocksr-libev
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/small-package
 mv package/small-package/luci-app-adguardhome package/luci-app-adguardhome
-#rm -rf feeds/packages/net/adguardhome
-#mv package/small-package/adguardhome feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/adguardhome
+mv package/small-package/adguardhome feeds/packages/net/adguardhome
 mv package/small-package/luci-app-ikoolproxy package/luci-app-ikoolproxy
 mv package/small-package/luci-app-alist package/luci-app-alist
 mv package/small-package/alist package/alist
