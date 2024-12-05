5d25c263fe8e3ae97d77355a56eaa1eb4b33244f
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-05 13:18:49 +0800

    Update hanwckf-mt798x.sh

--- script/hanwckf-mt798x.sh
+++ script/hanwckf-mt798x.sh
@@ -21,15 +21,15 @@ git clone --depth=1 https://github.com/xiangfeidexiaohuo/extra-ipk.git package/e
 mv package/extra-ipk/linkease package/linkease
 rm -rf package/extra-ipk
 
-
+git clone --depth=1 https://github.com/selfcan/luci-app-homebox.git package/luci-app-homebox
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
 git clone --depth=1 https://github.com/kenzok8/small-package.git package/small-package
 mv package/small-package/luci-app-adguardhome package/luci-app-adguardhome
 mv package/small-package/luci-app-ikoolproxy package/luci-app-ikoolproxy
-mv package/small-package/luci-app-alist package/luci-app-alist
-mv package/small-package/alist package/alist
-rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-alist
+#mv package/small-package/luci-app-alist package/luci-app-alist
+#mv package/small-package/alist package/alist
+#rm -rf feeds/packages/net/alist
+#rm -rf feeds/luci/applications/luci-app-alist
 rm -rf package/small-package
 
 git clone --depth=1 https://github.com/coolsnowwolf/lede.git package/lede
