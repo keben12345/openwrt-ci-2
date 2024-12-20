3010ac310dc04ea0ace155c64a92448e5af0d90e
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-20 16:23:05 +0800

    Update lean-x86.sh

--- script/lean-x86.sh
+++ script/lean-x86.sh
@@ -1,16 +1,14 @@
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.10.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
-#sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/EzOpWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/EzOpWrt/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/lean/defset package/lean/default-settings/files/zzz-default-settings
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-rm -rf feeds/smpackage/luci-app-advanced
-mv $GITHUB_WORKSPACE/patch/lean/99-msd_lite package/base-files/files/etc/99-msd_lite
-mv $GITHUB_WORKSPACE/patch/lean/99-udpxy package/base-files/files/etc/99-udpxy
+
 git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
 
 #下载5g模块
@@ -28,3 +26,23 @@ git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git f
 #git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 #git clone --depth=1 https://github.com/kiddin9/openwrt-clouddrive2.git  package/clouddrive2
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
+git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
+git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
+mv package/sirpdboy-package/luci-app-pppoe-server feeds/luci-app-pppoe-server
+mv package/sirpdboy-package/luci-app-socat feeds/luci-app-socat
+mv package/sirpdboy-package/luci-app-wolplus feeds/luci-app-wolplus
+rm -rf package/sirpdboy-package
+
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/luci/applications/luci-app-aria2
+mv feeds/smpackage/aria2 package/aria2
+mv feeds/smpackage/luci-app-aria2 package/luci-app-aria2
+
+rm -rf feeds/luci/applications/luci-app-socat
+rm -rf feeds/luci/applications/luci-app-pptp-server
+
+rm -rf feeds/smpackage/luci-app-pppoe-server
+rm -rf feeds/smpackage/luci-app-socat
+rm -rf feeds/smpackage/luci-app-wolplus
