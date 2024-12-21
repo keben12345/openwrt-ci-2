c1538cedc0f9b490ea8acfeca9a3df4f8368fc9c
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-21 19:43:18 +0800

    Update 7622-openwrt23.05.sh

--- script/7622-openwrt23.05.sh
+++ script/7622-openwrt23.05.sh
@@ -1,7 +1,8 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 
-
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
