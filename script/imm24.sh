0bbfdd691f0e6aaf0192d6453af65a3a081555fc
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-22 04:05:27 +0800

    Update imm24.sh

--- script/imm24.sh
+++ script/imm24.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/RPI4B/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/X86/g' package/base-files/files/bin/config_generate
 sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 mv $GITHUB_WORKSPACE/patch/banner $OPENWRT_PATH/package/base-files/files/etc/banner
 
