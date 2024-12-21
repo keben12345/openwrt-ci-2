b602a1dd11c08b8b9ec66e75017e184bacbd6f25
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-21 17:43:38 +0800

    Update 7622-openwrt23.05.sh

--- script/7622-openwrt23.05.sh
+++ script/7622-openwrt23.05.sh
@@ -5,3 +5,10 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+
+git clone --depth=1 -b openwrt-23.05 https://github.com/immortalwrt/luci.git package/imm-luci23
+mv package/imm-luci23/applications/luci-app-autoreboot package/luci-app-autoreboot
+sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-autoreboot/Makefile
+mv package/imm-luci23/applications/luci-app-ramfree package/luci-app-ramfree
+sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-ramfree/Makefile
+rm -rf package/imm-luci23
