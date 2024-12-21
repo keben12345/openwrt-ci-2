e412f18e917765c3a9067adaee7848d735f2aabc
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-21 17:15:04 +0800

    Create 7622-openwrt23.05.sh

---
+++ script/7622-openwrt23.05.sh
@@ -0,0 +1,7 @@
+sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
+
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
