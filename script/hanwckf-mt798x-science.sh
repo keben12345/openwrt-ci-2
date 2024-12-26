00f38ee34a58cf3718f18fd7bfa75414ac062657
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-27 01:07:48 +0800

    Update hanwckf-mt798x-science.sh

--- script/hanwckf-mt798x-science.sh
+++ script/hanwckf-mt798x-science.sh
@@ -53,12 +53,13 @@ git clone --depth=1 https://github.com/xiangfeidexiaohuo/extra-ipk.git package/e
 mv package/extra-ipk/linkease package/linkease
 rm -rf package/extra-ipk
 
-git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23pkg
-mv package/imm23pkg/net/frp package/frp
-rm -rf package/imm23pkg
 rm -rf feeds/packages/net/frp
+git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages package/imm23pkg
+mv package/imm23pkg/net/frp feeds/packages/net/frp
+rm -rf package/imm23pkg
 
-git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
-mv package/imm23luci/applications/luci-app-frpc package/luci-app-frpc
-rm -rf package/imm23luci
 rm -rf feeds/luci/applications/luci-app-frpc
+git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci package/imm23luci
+mv package/imm23luci/applications/luci-app-frpc feeds/luci/applications/luci-app-frpc
+rm -rf package/imm23luci
+
