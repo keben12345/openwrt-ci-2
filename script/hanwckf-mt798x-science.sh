d252868603f0301230fe92b0c301c401c49f262d
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2024-12-30 12:02:32 +0800

    Update hanwckf-mt798x-science.sh

--- script/hanwckf-mt798x-science.sh
+++ script/hanwckf-mt798x-science.sh
@@ -1,5 +1,5 @@
-sed -i 's/192.168.1.1/192.168.51.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.51.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.6.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/Routert/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/WiFi6/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
