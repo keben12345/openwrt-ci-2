5ad4a44b87e31b07e3b3d516a4f03fc0ab4adfc6
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-01-02 18:27:05 +0800

    Update lean-x86.sh

--- script/lean-x86.sh
+++ script/lean-x86.sh
@@ -17,7 +17,6 @@ git clone --depth=1 -b main https://github.com/linkease/istore.git package/istor
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh-cn/modem.po
 #sed -i 's/移动通信模组/通信模组/g' package/5g-modem/luci-app-modem/po/zh_Hans/modem.po
 
-
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
@@ -28,22 +27,6 @@ git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git f
 #git clone --depth=1 https://github.com/kiddin9/openwrt-clouddrive2.git  package/clouddrive2
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
-git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
-git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-mv package/sirpdboy-package/luci-app-pppoe-server feeds/luci-app-pppoe-server
-mv package/sirpdboy-package/luci-app-socat feeds/luci-app-socat
-mv package/sirpdboy-package/luci-app-wolplus feeds/luci-app-wolplus
-rm -rf package/sirpdboy-package
-
-rm -rf feeds/packages/net/aria2
-rm -rf feeds/luci/applications/luci-app-aria2
-mv feeds/smpackage/aria2 package/aria2
-mv feeds/smpackage/luci-app-aria2 package/luci-app-aria2
-
-rm -rf feeds/luci/applications/luci-app-socat
-rm -rf feeds/luci/applications/luci-app-pptp-server
-
-rm -rf feeds/smpackage/luci-app-pppoe-server
-rm -rf feeds/smpackage/luci-app-socat
-rm -rf feeds/smpackage/luci-app-wolplus
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages package/oppkg
+mv package/oppkg/luci-app-alist package/luci-app-alist
+rm -rf package/oppkg
