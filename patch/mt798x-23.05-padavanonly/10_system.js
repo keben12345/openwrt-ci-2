3ce1df5e4d677638719167c0b64b1647957f2786
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-02-06 15:41:05 +0800

    Update 10_system.js

--- patch/mt798x-23.05-padavanonly/10_system.js
+++ patch/mt798x-23.05-padavanonly/10_system.js
@@ -83,7 +83,7 @@ return baseclass.extend({
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
+			_('Firmware Version'), 'ImmortalWrt 23.05 TK防关联软路由Gi虾米定制系统 ',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
