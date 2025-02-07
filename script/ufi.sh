2dbbb57a4f8e8408db248ddbc57c31ca2a339fc3
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-02-07 15:48:35 +0800

    Update ufi.sh

--- script/ufi.sh
+++ script/ufi.sh
@@ -1,5 +1,6 @@
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+mv $GITHUB_WORKSPACE/patch/ufi/199-diy.sh package/base-files/files/etc/uci-defaults/199-diy.sh
 #完全删除luci版本
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 #添加编译日期
