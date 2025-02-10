954fc7f5de2486bd6e3111f4f498c3855e7a8a3c
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-02-10 16:32:40 +0800

    Update 10_system.js

--- patch/immortalwrt-23.05/10_system.js
+++ patch/immortalwrt-23.05/10_system.js
@@ -84,8 +84,7 @@ return baseclass.extend({
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
-			_('Kernel Version'),   boardinfo.kernel,
+			_('Firmware Version'), 'ImmortalWrt 23.05 TK防关联软路由Gi虾米定制系统 ',
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
 			_('Load Average'),     Array.isArray(systeminfo.load) ? '%.2f, %.2f, %.2f'.format(
