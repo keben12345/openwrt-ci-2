bd69b78a76617bb92e52231b61fe141be6209b31
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-02-17 20:08:29 +0800

    Update 199-diy.sh

--- patch/lean/199-diy.sh
+++ patch/lean/199-diy.sh
@@ -7,10 +7,13 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-#其他网络设置
+uci set luci.main.mediaurlbase=/luci-static/design
+uci commit luci
 
+#其他网络设置
 uci set network.lan.ip6ifaceid='eui64'
 uci set network.lan.ipaddr=192.168.5.1
+uci commit network
 
 uci commit
 
