6e598a66b7be79612a9b2b1356686f46e80626cf
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-02-18 20:39:29 +0800

    Update 199-mydef.sh

--- patch/immortalwrt-24.10/199-mydef.sh
+++ patch/immortalwrt-24.10/199-mydef.sh
@@ -1,32 +1,5 @@
 #!/bin/sh
 
-# 设置默认防火墙规则，方便虚拟机首次访问 WebUI
-#uci set firewall.@zone[1].input='ACCEPT'
-
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-#uci add dhcp domain
-#uci set "dhcp.@domain[-1].name=time.android.com"
-#uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
-
-# 根据网卡数量配置网络
-count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    count=$((count + 1))
-  fi
-done
-
-# 网络设置
-if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
-  uci set dhcp.lan.ignore='1'
-elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ip6assign='64'
-fi
-
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
@@ -35,17 +8,10 @@ uci set dropbear.@dropbear[0].Interface=''
 
 #其他网络设置
 
-uci set network.lan.ip6ifaceid='eui64'
-
-uci set network.lan.ipaddr=192.168.2.2
-uci set network.lan.gateway=192.168.2.1
-uci set network.lan.dns=210.21.196.6
-uci set dhcp.lan.ignore=1
-
 uci commit
 
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 
-/etc/init.d/network restart
+#/etc/init.d/network restart
 
 exit 0
