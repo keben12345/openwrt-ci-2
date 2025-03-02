4b9922b7432d5a88a39dbda14b811a144d1d0e0a
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-03-02 23:14:08 +0800

    Update 199-diy.sh

--- patch/lean/199-diy.sh
+++ patch/lean/199-diy.sh
@@ -7,8 +7,8 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-uci set luci.main.mediaurlbase=/luci-static/design
-uci commit luci
+#uci set luci.main.mediaurlbase=/luci-static/design
+#uci commit luci
 
 #其他网络设置
 uci set network.lan.ip6ifaceid='eui64'
@@ -17,16 +17,17 @@ uci commit network
 
 uci commit
 
-date_version=$(date +"%y.%m.%d")
+date_version=$(date +"%Y.%m.%d")
 sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
+#echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
+echo "DISTRIB_REVISION='${date_version}'" >> /etc/openwrt_release
 sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt-Gi '" >> /etc/openwrt_release
 
 sed -i '/core/d' /etc/opkg/distfeeds.conf
 sed -i '/smpackage/d' /etc/opkg/distfeeds.conf
 sed -i 's#downloads.openwrt.org#mirror.nju.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i 's#packages/aarch64_cortex-a53/luci#packages-18.06-k5.4/aarch64_cortex-a53/luci#g' /etc/opkg/distfeeds.conf
+sed -i 's#24.10.0/packages/aarch64_cortex-a53/luci#18.06-k5.4-SNAPSHOT/packages/aarch64_cortex-a53/luci#g' /etc/opkg/distfeeds.conf
 
 /etc/init.d/network restart
 
