9878a1601daa650e2df00d3f048857f779fe3e02
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-03-04 14:51:52 +0800

    Update imm-mt798x.sh

--- script/imm-mt798x.sh
+++ script/imm-mt798x.sh
@@ -21,7 +21,7 @@ rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/devel/gn
 rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,geoview,hysteria,ipt2socks,microsocks,naiveproxy,pdnsd-alt,shadowsocks-rust,shadowsocksr-libev,simple-obfs,sing-box,ssocks,tcping,trojan*,tuic-client,v2ray*,xray*,mosdns}
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-openclash}
-git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 git clone --depth 1 https://github.com/vernesong/OpenClash.git  package/openclash
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
