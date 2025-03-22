e969820e3bef5d91b8f72a76b0825011cb896d49
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-03-22 17:58:17 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -9,6 +9,11 @@ uci set dropbear.@dropbear[0].Interface=''
 uci commit
 
 #sed -i 's/root::0:0:99999:7:::/root:$1$R43zhcLV$dwxtXXFI7TdIRcVKl.w3N1:20158:0:99999:7:::/g' /etc/shadow
+
+tar -zxf /etc/clash-linux-arm64.tar.gz -C /etc/openclash/core/
+mv /etc/openclash/core/clash /etc/openclash/core/clash_meta
+rm -rf /etc/clash-linux-arm64.tar.gz
+
 #/etc/init.d/network restart
 
 exit 0
