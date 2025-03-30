f0487d951310ca2ee0694b5793ac97d6d44a049f
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-03-30 22:13:05 +0800

    Update hanwckf-mt798x-science.sh

--- script/hanwckf-mt798x-science.sh
+++ script/hanwckf-mt798x-science.sh
@@ -59,6 +59,8 @@ rm -rf package/helloworld/shadowsocksr-libev
 rm -rf package/helloworld/gn
 
 rm -rf feeds/packages/net/v2ray-geodata
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
