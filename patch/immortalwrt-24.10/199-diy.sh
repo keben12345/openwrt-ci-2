fc96fdf0e70f5f764f8852b19534fc552790aaa1
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-03 16:25:02 +0800

    Update 199-diy.sh

--- patch/immortalwrt-24.10/199-diy.sh
+++ patch/immortalwrt-24.10/199-diy.sh
@@ -20,6 +20,37 @@ if [ -f "$OPENCLASH_FILE" ]; then
     rm -rf /etc/clash-linux-arm64.tar.gz
 fi
 
-#/etc/init.d/network restart
+# 统计eth接口数量，大于1个则将eth0设为wan其它网口设为lan，只有1个则设置成DHCP模式
+eth_count=$(ls /sys/class/net | grep -c '^eth')
+if [ $eth_count -gt 1 ]; then
+    uci set network.lan.ipaddr='192.168.23.1'
+
+    uci del dhcp.lan.ra_slaac
+    uci del dhcp.lan.dhcpv6
+    uci del dhcp.lan.ra_flags
+    uci add_list dhcp.lan.ra_flags='none'
+    uci set dhcp.lan.dns_service='0'
+
+    uci del network.wan6
+    uci del network.globals.packet_steering
+    uci del network.globals.ula_prefix
+    uci set network.lan.ip6assign='64'
+    uci set network.lan.ip6ifaceid='eui64'
+
+    uci set network.wan.device='eth0'
+    uci del network.cfg030f15.ports
+    ls /sys/class/net | awk '/^eth[0-9]+$/ && $0 != "eth0" {print "uci add_list network.cfg030f15.ports="$0}' | sh   
+else
+    uci set network.lan.proto='dhcp'
+    uci set dhcp.lan.ignore='1'
+fi
+
+#ls /sys/class/net | grep -E '^eth[0-9]+$' | grep -v '^eth0$' | sed 's/^/uci add_list network.cfg030f15.ports=/' | sh
+
+uci commit dhcp
+uci commit network
+uci commit
+/etc/init.d/network restart
+/etc/init.d/odhcpd restart
 
 exit 0
