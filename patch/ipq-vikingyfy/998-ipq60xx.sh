55c9f5ecae5fc5e1517749129ead9eb20ca0772d
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-05 19:21:05 +0800

    Update 998-ipq60xx.sh

--- patch/ipq-vikingyfy/998-ipq60xx.sh
+++ patch/ipq-vikingyfy/998-ipq60xx.sh
@@ -5,8 +5,7 @@ sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
 #sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.77-1-51630055abc52e4640365e0d2a28a72e/packages.adb' /etc/apk/repositories.d/customfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.84-1-ea69bec1cc40cedb58b57bb61fc65eab/packages.adb' /etc/apk/repositories.d/customfeeds.list
-
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.6.85-1-633ed8724402e6b10849827a8dc66a67/packages.adb' /etc/apk/repositories.d/customfeeds.list
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
@@ -14,9 +13,9 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 # wifi设置
-#uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
-#uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
-
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci commit wireless
 uci commit
 
 OPENCLASH_FILE="/etc/config/openclash"
