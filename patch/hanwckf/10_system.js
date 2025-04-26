6b4f1f2de02083baec3aa20af79a5dbcd7c6e6b6
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-26 22:57:16 +0800

    Update 10_system.js

--- patch/hanwckf/10_system.js
+++ patch/hanwckf/10_system.js
@@ -80,10 +80,8 @@ return baseclass.extend({
 
 		var fields = [
 			_('Hostname'),         boardinfo.hostname,
-			_('Model'),            boardinfo.model + cpubench.cpubench,
-			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), 'ImmortalWrt 21.02-SNAPSHOT / 轻音专用 ',
+			_('Firmware Version'), 'ImmortalWrt-21.02 / TikTok专用 ',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
