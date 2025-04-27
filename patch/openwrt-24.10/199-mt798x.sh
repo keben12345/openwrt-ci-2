b04ee560d06f8c23975738680a11192b30a9873c
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-27 16:12:02 +0800

    Create 199-mt798x.sh

---
+++ patch/openwrt-24.10/199-mt798x.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+
+uci commit
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
+
+OPENCLASH_FILE="/etc/config/openclash"
+if [ -f "$OPENCLASH_FILE" ]; then
+    mv /etc/my-clash /etc/openclash/core/clash_meta
+fi
+
+#uci commit dhcp
+#uci commit network
+#uci commit
+#/etc/init.d/network restart
+#/etc/init.d/odhcpd restart
+
+exit 0
