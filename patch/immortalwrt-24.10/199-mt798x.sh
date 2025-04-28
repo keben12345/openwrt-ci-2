4a64c32c8b716d4a2630c05b8561f62e60017efa
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-28 16:32:56 +0800

    Update 199-mt798x.sh

--- patch/immortalwrt-24.10/199-mt798x.sh
+++ patch/immortalwrt-24.10/199-mt798x.sh
@@ -12,7 +12,8 @@ uci commit
 
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
-sed -i 's#downloads.immortalwrt.org#mirrors.nju.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/mediatek/filogic/kmods/6.6.86-1-f2c63d42896f12da2b55f2c2b626e7be' /etc/opkg/distfeeds.conf
 
 OPENCLASH_FILE="/etc/config/openclash"
 if [ -f "$OPENCLASH_FILE" ]; then
