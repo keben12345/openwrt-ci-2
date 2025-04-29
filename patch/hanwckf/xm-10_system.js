e9e8df50aabdf4000abc6a412fc04c36544df1fe
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-04-29 15:58:55 +0800

    Update and rename 10_system (1).js to xm-10_system.js

--- patch/hanwckf/xm-10_system.js
+++ patch/hanwckf/xm-10_system.js
@@ -83,7 +83,7 @@ return baseclass.extend({
 			_('Model'),            boardinfo.model + cpubench.cpubench,
 			_('Architecture'),     cpuinfo.cpuinfo || boardinfo.system,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
-			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
+			_('Firmware Version'), 'TikTok专用防关联路由器 / 虾米定制版',
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
