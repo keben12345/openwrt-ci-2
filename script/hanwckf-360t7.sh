d6d07d0d55bf2c2b6db79371571c3b4f2b367fb5
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-02 17:03:08 +0800

    Update hanwckf-360t7.sh

--- script/hanwckf-360t7.sh
+++ script/hanwckf-360t7.sh
@@ -12,6 +12,7 @@ if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
 fi
 
 mv $GITHUB_WORKSPACE/patch/hanwckf/199-mydiy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
+mv $GITHUB_WORKSPACE/patch/hanwckf/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 #完全删除luci版本
 sed -i "s/+ ' \/ ' : '') + (luciversion ||/:/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
