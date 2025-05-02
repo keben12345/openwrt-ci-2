981824e5a920bcc8efaede826cb3733f61904792
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-03 06:57:56 +0800

    Update hanwckf-360t7.sh

--- script/hanwckf-360t7.sh
+++ script/hanwckf-360t7.sh
@@ -1,6 +1,6 @@
 sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.13.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/Router/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/WiFi/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 
