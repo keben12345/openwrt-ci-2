384bff5a7eb5eaf66e6b634394b8a7af755e34f4
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-12 16:17:35 +0800

    Create 199-x86.sh

---
+++ patch/immortalwrt-24.10/199-x86.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+
+uci commit
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-a99af258b23725bab7a4c5448b90efca' /etc/opkg/distfeeds.conf
+
+OPENCLASH_FILE="/etc/config/openclash"
+if [ -f "$OPENCLASH_FILE" ]; then
+    mv /etc/my-clash /etc/openclash/core/clash_meta
+fi
+
+uci commit
+
+exit 0
