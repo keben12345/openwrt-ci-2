95110acca052afe1212bad2b255ec61cf2106163
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-14 16:17:07 +0800

    Update 199-x86.sh

--- patch/immortalwrt-24.10/199-x86.sh
+++ patch/immortalwrt-24.10/199-x86.sh
@@ -14,6 +14,7 @@ sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-a99af258b23725bab7a4c5448b90efca' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/x86_64/kiddin9' /etc/opkg/customfeeds.conf
 
 OPENCLASH_FILE="/etc/config/openclash"
 if [ -f "$OPENCLASH_FILE" ]; then
