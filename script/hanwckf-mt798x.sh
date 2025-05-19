01d3f79f4d67473ce89ff993ec63b9197bf248a3
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-19 21:37:15 +0800

    Update hanwckf-mt798x.sh

--- script/hanwckf-mt798x.sh
+++ script/hanwckf-mt798x.sh
@@ -42,6 +42,7 @@ rm -rf feeds/packages/net/ua2f
 #git clone --depth 1 https://github.com/mchome/openwrt-dogcom.git package/openwrt-dogcom
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/oaf
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
+git clone --depth 1 https://github.com/5und4y2023/luci-app-ddns-go.git package/luci-app-ddns-go
 
 #下载5g模块
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
