be51970a0182a1f33bf19d4b4a68e61d6162787a
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-05-31 21:47:44 +0800

    Create 199-x86-pang.sh

---
+++ patch/immortalwrt-24.10/199-x86-pang.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+#其他网络设置
+
+uci commit
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-a99af258b23725bab7a4c5448b90efca' /etc/opkg/distfeeds.conf
+sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/x86_64/kiddin9' /etc/opkg/customfeeds.conf
+
+OPENCLASH_FILE="/etc/config/openclash"
+if [ -f "$OPENCLASH_FILE" ]; then
+    mv /etc/my-clash /etc/openclash/core/clash_meta
+fi
+
+# 统计eth接口数量，大于1个则将eth0设为wan其它网口设为lan，只有1个则设置成DHCP模式
+eth_count=$(ls /sys/class/net | grep -c '^eth')
+if [ $eth_count -gt 1 ]; then
+    uci del network.wan
+    uci del network.wan6
+    uci del network.cfg030f15.ports
+    uci add_list network.cfg030f15.ports='eth0'
+    ls /sys/class/net | awk '/^eth[0-9]+$/ && $0 != "eth0" {print "uci add_list network.cfg030f15.ports="$0}' | sh   
+    uci set network.lan.proto='dhcp'
+    uci set dhcp.lan.ignore='1'
+else
+    uci set network.lan.proto='dhcp'
+    uci set dhcp.lan.ignore='1'
+fi
+
+uci commit dhcp
+uci commit network
+uci commit
+/etc/init.d/network restart
+/etc/init.d/odhcpd restart
+
+exit 0
