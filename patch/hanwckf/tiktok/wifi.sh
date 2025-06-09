9cdda3cd9983b3cdd043e8ea4ffb651d2d7094b4
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-06-10 02:57:45 +0800

    Create wifi.sh

---
+++ patch/hanwckf/tiktok/wifi.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+wifi_ssid=$(cat /sys/class/net/br-lan/address 2>/dev/null | awk -F: '{print $5 $6}' | tr 'a-z' 'A-Z')-WiFi
+wifi_num=15
+wifi_password=123456789
+
+# 生成配置
+for i in $(seq 1 $wifi_num); do
+    wifinet_num=$((i + 1))
+
+    # 根据序号选择wireless设备
+    if [ $i -le 15 ]; then
+        wireless_dev="MT7986_1_2"
+		network_dev="rax${i}"
+    else
+        wireless_dev="MT7986_1_1"
+		network_dev="ra$((i - 15))"
+    fi
+
+    # 配置无线接口
+    uci set wireless.wifinet${wifinet_num}=wifi-iface
+    uci set wireless.wifinet${wifinet_num}.device="$wireless_dev"
+    uci set wireless.wifinet${wifinet_num}.mode='ap'
+    uci set wireless.wifinet${wifinet_num}.ssid="${wifi_ssid}-${i}"
+    uci set wireless.wifinet${wifinet_num}.encryption='psk2+ccmp'
+    uci set wireless.wifinet${wifinet_num}.key="$wifi_password"
+    # uci set wireless.wifinet${wifinet_num}.ifname="ap${i}"
+    uci set wireless.wifinet${wifinet_num}.network="wifi${i}"
+
+    # 配置网络接口
+    uci set network.wifi${i}=interface
+    uci set network.wifi${i}.proto='static'
+    uci set network.wifi${i}.device="${network_dev}"
+    uci set network.wifi${i}.ipaddr="10.10.${i}.1"
+    uci set network.wifi${i}.netmask='255.255.255.0'
+
+    # 配置DHCP
+    uci set dhcp.wifi${i}=dhcp
+    uci set dhcp.wifi${i}.interface="wifi${i}"
+
+    # 配置防火墙
+    uci add firewall zone
+    uci set firewall.@zone[-1].name="wifi${i}"
+    uci set firewall.@zone[-1].input='ACCEPT'
+    uci set firewall.@zone[-1].output='ACCEPT'
+    uci set firewall.@zone[-1].forward='ACCEPT'
+    uci add_list firewall.@zone[-1].network="wifi${i}"
+    uci add firewall forwarding
+    uci set firewall.@forwarding[-1].src="wifi${i}"
+    uci set firewall.@forwarding[-1].dest='wan'
+    
+    uci add firewall rule
+    uci set firewall.@rule[-1].src="wifi${i}"
+    uci set firewall.@rule[-1].dest='wan'
+    uci set firewall.@rule[-1].name="wifi${i}"
+    uci add_list firewall.@rule[-1].proto='all'
+    uci set firewall.@rule[-1].target='REJECT'
+done
+
+# 提交配置
+uci commit wireless
+uci commit network
+uci commit dhcp
+uci commit firewall
+
+# 重启服务
+/etc/init.d/network restart >/dev/null 2>&1
+/etc/init.d/firewall restart >/dev/null 2>&1
+/etc/init.d/dnsmasq restart >/dev/null 2>&1
+/etc/init.d/dropbear restart >/dev/null 2>&1
