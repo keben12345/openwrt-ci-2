74b7a4ab3f2cafd2934a394ceaa62dae42635b07
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-06-10 21:12:13 +0800

    Update lean-23.05.sh

--- script/lean-23.05.sh
+++ script/lean-23.05.sh
@@ -1,16 +1,16 @@
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.15.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/luci2/bin/config_generate
-sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
-sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/QINGYIN/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/QINGYIN/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/QINGYIN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/tencent.com/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '/shadow/d' package/lean/default-settings/files/zzz-default-settings
 
 mv $GITHUB_WORKSPACE/patch/lean/199-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
-sed -i 's/0x0580000 0x7280000/0x580000 0x1cc00000/g' target/linux/mediatek/dts/mt7986a-netcore-n60-pro.dts
+# sed -i 's/0x0580000 0x7280000/0x580000 0x1cc00000/g' target/linux/mediatek/dts/mt7986a-netcore-n60-pro.dts
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
@@ -27,6 +27,9 @@ fi
 mv $GITHUB_WORKSPACE/patch/QINGYIN/QINGYINSSIDMAC1.sh package/base-files/files/etc/QINGYINSSIDMAC1.sh
 mv $GITHUB_WORKSPACE/patch/QINGYIN/lede-10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 
+find ./ | grep Makefile | grep oaf | xargs rm -f
+rm -rf feeds/packages/net/open-app-filter
+git clone --depth 1 https://github.com/destan19/OpenAppFilter.git  package/oaf
 git clone --depth 1 https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 git clone --depth 1 https://github.com/lwb1978/openwrt-gecoosac.git package/openwrt-gecoosac
 
@@ -53,13 +56,15 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 rm -rf feeds/packages/net/{chinadns-ng,dns2socks,geoview,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
 rm -rf feeds/packages/net/{simple-obfs,sing-box,tcping,trojan-plus,tuic-client,v2ray-geodata,v2ray-plugin,xray-core,xray-plugin}
 rm -rf feeds/packages/net/{dns2socks-rust,dns2tcp,dnsproxy,gn,redsocks2,shadow-tls,trojan,v2ray-core}
-rm -rf feeds/packages/net/{mosdns,adguardhome,lucky,smartdns}
-rm -rf feeds/luci/applications/{luci-app-alist,luci-app-lucky}
+rm -rf feeds/packages/net/{adguardhome,mosdns,lucky,tailscale}
+rm -rf feeds/luci/applications/{luci-app-alist,luci-app-lucky,luci-app-tailscale}
 git clone --depth 1 https://github.com/kenzok8/small-package.git package/kz8-small
 mv package/kz8-small/adguardhome package/adguardhome
 mv package/kz8-small/luci-app-adguardhome package/luci-app-adguardhome
 mv package/kz8-small/lucky package/lucky
 mv package/kz8-small/luci-app-lucky package/luci-app-lucky
-mv package/kz8-small/smartdns package/smartdns
+mv package/kz8-small/luci-app-wrtbwmon package/luci-app-wrtbwmon
+mv package/kz8-small/wrtbwmon package/wrtbwmon
+mv package/kz8-small/luci-app-tailscale package/luci-app-tailscale
+mv package/kz8-small/tailscale package/tailscale
 rm -rf package/kz8-small
-
