8bf75dd259eb68dd73c96d0928e0dd96736540fa
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-06-23 16:26:15 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -25,13 +25,13 @@ uci commit network
 uci commit
 
 #uci set wireless.default_MT7981_1_1.ssid=xiaoguo
-uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_1.key=123456qwerty
+#uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_1.key=123456qwerty
 
 #uci set wireless.default_MT7981_1_2.ssid=TK888
-uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_2.key=123456qwerty
-uci commit wireless
+#uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_2.key=123456qwerty
+#uci commit wireless
 uci commit
 
 #sed -i 's/root::0:0:99999:7:::/root:$1$0kv2aZ4P$WkI.7M.V1N6WSEDahJwot.:0:0:99999:7:::/g' /etc/shadow
