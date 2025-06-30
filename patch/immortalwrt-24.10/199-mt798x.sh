f39a921204a4fcfbaf28c28990df102dea727255
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-06-30 22:55:02 +0800

    Update 199-mt798x.sh

--- patch/immortalwrt-24.10/199-mt798x.sh
+++ patch/immortalwrt-24.10/199-mt798x.sh
@@ -7,20 +7,73 @@ uci delete ttyd.@ttyd[0].interface
 uci set dropbear.@dropbear[0].Interface=''
 
 #其他网络设置
-
+uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
+uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
+uci set wireless.default_radio1.encryption='psk2+ccmp'
+uci set wireless.default_radio0.encryption='psk2+ccmp'
+uci set wireless.default_radio1.key='88888888'
+uci set wireless.default_radio0.key='88888888'
+uci commit wireless
 uci commit
+
+uci set wireless.wifinet2=wifi-iface
+uci set wireless.wifinet2.device=radio1
+uci set wireless.wifinet2.mode='ap'
+uci set wireless.wifinet2.ssid='Tk001'
+uci set wireless.wifinet2.encryption='psk2+ccmp'
+uci set wireless.wifinet2.key='88888888'
+uci set wireless.wifinet2.ifname='ap1'
+uci set wireless.wifinet2.network='tk001'
+
+# 配置网络接口
+uci set network.wifi${i}=interface
+uci set network.wifi${i}.proto='static'
+uci set network.wifi${i}.device='ap1'
+uci set network.wifi${i}.ipaddr='10.10.1.1'
+uci set network.wifi${i}.netmask='255.255.255.0'
+
+# 配置DHCP
+uci set dhcp.wifi${i}=dhcp
+uci set dhcp.wifi${i}.interface='tk001'
+
+# 配置防火墙
+uci add firewall zone
+uci set firewall.@zone[-1].name='tk001'
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+uci add_list firewall.@zone[-1].network='tk001'
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='tk001'
+uci set firewall.@forwarding[-1].dest='wan'
+
+uci add firewall rule
+uci set firewall.@rule[-1].src='tk001'
+uci set firewall.@rule[-1].dest='wan'
+uci set firewall.@rule[-1].name='tk001'
+uci add_list firewall.@rule[-1].proto='all'
+uci set firewall.@rule[-1].target='REJECT'
+done
+
+# 提交配置
+uci commit wireless
+uci commit network
+uci commit dhcp
+uci commit firewall
 
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/mediatek/filogic/kmods/6.6.86-1-f2c63d42896f12da2b55f2c2b626e7be' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.2/targets/mediatek/filogic/kmods/6.6.93-1-2ccac7a75355327cb6dfb4df1ecb575e' /etc/opkg/distfeeds.conf
 
 OPENCLASH_FILE="/etc/config/openclash"
 if [ -f "$OPENCLASH_FILE" ]; then
     mv /etc/my-clash /etc/openclash/core/clash_meta
 fi
 
-#/etc/init.d/network restart
-#/etc/init.d/odhcpd restart
+/etc/init.d/network restart >/dev/null 2>&1
+/etc/init.d/firewall restart >/dev/null 2>&1
+/etc/init.d/dnsmasq restart >/dev/null 2>&1
+/etc/init.d/dropbear restart >/dev/null 2>&1
 
 exit 0
