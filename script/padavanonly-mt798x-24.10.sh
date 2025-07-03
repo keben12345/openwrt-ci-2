537a68e3d43bcfb485b3b70e4b1121319987506e
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-03 19:05:38 +0800

    Update padavanonly-mt798x-24.10.sh

--- script/padavanonly-mt798x-24.10.sh
+++ script/padavanonly-mt798x-24.10.sh
@@ -9,10 +9,13 @@ sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emo
 #sed -i 's/luci-app-usb-printer luci-i18n-usb-printer-zh-cn/kmod-usb-storage/g' target/linux/mediatek/image/mt7981.mk
 # mv $GITHUB_WORKSPACE/patch/padavanonly/libxcrypt-Makefile feeds/packages/libs/libxcrypt/Makefile
 
+rm -rf /include/kernel-6.6
+mv $GITHUB_WORKSPACE/patch/padavanonly/kernel-6.6 include/kernel-6.6
+mv $GITHUB_WORKSPACE/patch/padavanonly/199-diy-ipv6.sh package/base-files/files/etc/uci-defaults/990-diy.sh
+
 # sed -i 's/0x580000 0x7280000/0x580000 0x1cc00000/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-netcore-n60-pro.dts
 
-# mv $GITHUB_WORKSPACE/patch/padavanonly/199-diy.sh package/base-files/files/etc/uci-defaults/199-diy.sh
-mv $GITHUB_WORKSPACE/patch/padavanonly/199-diy-ipv6.sh package/base-files/files/etc/uci-defaults/199-diy.sh
+
 # mv $GITHUB_WORKSPACE/patch/padavanonly/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 # mv $GITHUB_WORKSPACE/patch/padavanonly/QINGYINSSIDMAC1.sh package/base-files/files/etc/QINGYINSSIDMAC1.sh
 #完全删除luci版本
