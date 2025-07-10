70ef3d2c68160e761688f09c0ffb7447241ac440
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-11 02:27:28 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -37,10 +37,8 @@ uci commit
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
+
 
 /etc/init.d/network restart
 
