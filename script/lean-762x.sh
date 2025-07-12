739932ab13df89247b48521afc36c0e93c325804
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-12 21:24:28 +0800

    Update lean-762x.sh

--- script/lean-762x.sh
+++ script/lean-762x.sh
@@ -3,8 +3,8 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/lu
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
-#sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/LEDE/TIKTOK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/LEDE/TIKTOK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 mv $GITHUB_WORKSPACE/patch/lean/199-diy package/base-files/files/etc/uci-defaults/zz-diy
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 sed -i '/openwrt_release/d' package/lean/default-settings/files/zzz-default-settings
