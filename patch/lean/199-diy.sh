692bc3ba44bc8b2164e3aac5b0e3ea12d9f3137c
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-17 18:43:59 +0800

    Update 199-diy.sh

--- patch/lean/199-diy.sh
+++ patch/lean/199-diy.sh
@@ -17,23 +17,23 @@ uci set dropbear.@dropbear[0].Interface=''
 
 uci commit
 
-date_version=$(date +"%Y.%m.%d")
-#sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-#echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
-#sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
-#echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
-
+sed -i 's#mirrors.tencent.com/lede#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+#sed -i 's#packages/aarch64_cortex-a53/luci#packages-18.06-k5.4/aarch64_cortex-a53/luci#g' /etc/opkg/distfeeds.conf
 sed -i '/core/d' /etc/opkg/distfeeds.conf
 sed -i '/smpackage/d' /etc/opkg/distfeeds.conf
-sed -i 's#downloads.openwrt.org#mirror.nju.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-#sed -i 's#24.10.0/packages/aarch64_cortex-a53/luci#18.06-k5.4-SNAPSHOT/packages/aarch64_cortex-a53/luci#g' /etc/opkg/distfeeds.conf
+sed -i '/kwrt/d' /etc/opkg/distfeeds.conf
+#sed -i '/luci/d' /etc/opkg/distfeeds.conf
+#sed -i '$a src/gz openwrt_luci https://mirrors.pku.edu.cn/immortalwrt/releases/18.06-k5.4-SNAPSHOT/packages/mipsel_24kc/luci' /etc/opkg/distfeeds.conf
+#sed -i '$a src/gz openwrt_luci https://mirrors.pku.edu.cn/immortalwrt/releases/18.06-k5.4-SNAPSHOT/packages/aarch64_cortex-a53/luci' /etc/opkg/distfeeds.conf
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+date_version=$(date +"%Y.%m.%d")
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+echo "DISTRIB_REVISION='V${date_version}'" >> /etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='OpenWrt  '" >> /etc/openwrt_release
 
-chmod 644 /etc/init.d/QINGYINSSIDMAC1.sh
+cp /etc/my-clash /etc/openclash/core/clash_meta
+# chmod 644 /etc/init.d/QINGYINSSIDMAC1.sh
 
 #/etc/init.d/network restart
 
