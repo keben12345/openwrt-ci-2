0d656623e1fdb1d2d852642fcabb59d05f9802e7
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-26 03:40:41 +0800

    Update zz-diy.sh

--- patch/bw/zz-diy.sh
+++ patch/bw/zz-diy.sh
@@ -37,9 +37,80 @@ uci commit
 sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
-cp /etc/my-clash /etc/openclash/core/clash_meta
+# cp /etc/my-clash /etc/openclash/core/clash_meta
 
 
-# /etc/init.d/network restart
+num=5
+wifipassword=password
+ipc=1
+
+# 生成配置
+for i in $(seq 1 $num); do
+    wifinet_num=$((i + 1))
+    new_c=$((ipc + i -1))
+    ipaddr="10.10.${new_c}.1"
+
+    # 根据序号选择wireless设备
+    if [ $i -le 15 ]; then
+        wireless_dev="MT7986_1_2"
+		network_dev="rax${i}"
+    else
+        wireless_dev="MT7986_1_1"
+		network_dev="ra$((i - 15))"
+    fi
+
+    # 配置无线接口
+    uci set wireless.wifinet${wifinet_num}=wifi-iface
+    uci set wireless.wifinet${wifinet_num}.device="$wireless_dev"
+    uci set wireless.wifinet${wifinet_num}.mode='ap'
+    uci set wireless.wifinet${wifinet_num}.ssid="dh-${i}"
+    uci set wireless.wifinet${wifinet_num}.encryption='psk2+ccmp'
+    uci set wireless.wifinet${wifinet_num}.key="$wifipassword"
+    # uci set wireless.wifinet${wifinet_num}.ifname="ap${i}"
+    uci set wireless.wifinet${wifinet_num}.network="wifi${i}"
+
+    # 配置网络接口
+    uci set network.wifi${i}=interface
+    uci set network.wifi${i}.proto='static'
+    uci set network.wifi${i}.device="${network_dev}"
+    uci set network.wifi${i}.ipaddr="$ipaddr"
+    uci set network.wifi${i}.netmask='255.255.255.0'
+
+    # 配置DHCP
+    uci set dhcp.wifi${i}=dhcp
+    uci set dhcp.wifi${i}.interface="wifi${i}"
+
+done
+
+    # 配置防火墙
+uci add firewall zone
+uci set firewall.@zone[-1].name="proxy"
+uci set firewall.@zone[-1].input='ACCEPT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='ACCEPT'
+uci add_list firewall.@zone[-1].network="proxy"
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src="proxy"
+uci set firewall.@forwarding[-1].dest='wan'
+    
+uci add firewall rule
+uci set firewall.@rule[-1].src="proxy"
+uci set firewall.@rule[-1].dest='wan'
+uci set firewall.@rule[-1].name="proxy"
+uci add_list firewall.@rule[-1].proto='all'
+uci set firewall.@rule[-1].target='REJECT'
+
+# 提交配置
+uci commit wireless
+uci commit network
+uci commit dhcp
+uci commit firewall
+
+#cp /etc/my-clash /etc/openclash/core/clash_meta
+
+/etc/init.d/network restart >/dev/null 2>&1
+/etc/init.d/firewall restart >/dev/null 2>&1
+/etc/init.d/dnsmasq restart >/dev/null 2>&1
+/etc/init.d/dropbear restart >/dev/null 2>&1
 
 exit 0
