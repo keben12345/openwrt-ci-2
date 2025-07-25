a15320884f8e9f77c2beabee5f8c3d5f2ba61216
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-07-26 03:25:08 +0800

    Update hanwckf-mt798x-bw.sh

--- script/hanwckf-mt798x-bw.sh
+++ script/hanwckf-mt798x-bw.sh
@@ -1,4 +1,4 @@
-sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
