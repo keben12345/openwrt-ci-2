c4a0405fc4efdce81f9a8e1d69871ac1f6265e94
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-08-06 02:03:54 +0800

    Update 998-ipq60xx.sh

--- patch/ipq-vikingyfy/998-ipq60xx.sh
+++ patch/ipq-vikingyfy/998-ipq60xx.sh
@@ -5,7 +5,7 @@ sed -i '/sqm/d' /etc/apk/repositories.d/distfeeds.list
 sed -i '/qualcommax/d' /etc/apk/repositories.d/distfeeds.list
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/apk/repositories.d/distfeeds.list
 sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/packages/packages.adb' /etc/apk/repositories.d/distfeeds.list
-sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.35-1-d0229791e1a081cf44229b6bac87670d/packages.adb' /etc/apk/repositories.d/distfeeds.list
+sed -i '$a https://mirrors.pku.edu.cn/immortalwrt/snapshots/targets/qualcommax/ipq60xx/kmods/6.12.40-1-b8a3862478272101f3e55498ae455909/packages.adb' /etc/apk/repositories.d/distfeeds.list
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
@@ -20,6 +20,6 @@ uci set wireless.radio1.txpower='20'
 uci commit wireless
 uci commit
 
-/etc/init.d/network restart
+#不用重启/etc/init.d/network restart
 
 exit 0
