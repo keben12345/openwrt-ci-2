1c5b4915b005c0efdfe6fdc288cfb8e22558d660
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-08-07 02:20:53 +0800

    Update 199-x86.sh

--- patch/immortalwrt-24.10/199-x86.sh
+++ patch/immortalwrt-24.10/199-x86.sh
@@ -13,30 +13,14 @@ uci commit
 sed -i '/modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-a99af258b23725bab7a4c5448b90efca' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10.2/targets/x86/64/kmods/6.6.93-1-c69323a377952557bd871e9f3c2e82ae' /etc/opkg/distfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/x86_64/kiddin9' /etc/opkg/customfeeds.conf
 
-OPENCLASH_FILE="/etc/config/openclash"
-if [ -f "$OPENCLASH_FILE" ]; then
-    mv /etc/my-clash /etc/openclash/core/clash_meta
-fi
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
-# 统计eth接口数量，大于1个则将eth0设为wan其它网口设为lan，只有1个则设置成DHCP模式
-eth_count=$(ls /sys/class/net | grep -c '^eth')
-if [ $eth_count -gt 1 ]; then
-    uci set network.wan.device='eth0'
-    uci set network.wan6.device='eth0'
-    uci del network.cfg030f15.ports
-    ls /sys/class/net | awk '/^eth[0-9]+$/ && $0 != "eth0" {print "uci add_list network.cfg030f15.ports="$0}' | sh   
-else
-    uci set network.lan.proto='dhcp'
-    uci set dhcp.lan.ignore='1'
-fi
 
-uci commit dhcp
-uci commit network
 uci commit
-/etc/init.d/network restart
-/etc/init.d/odhcpd restart
+#/etc/init.d/network restart
+#/etc/init.d/odhcpd restart
 
 exit 0
