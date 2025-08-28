9e9b9196648c2f38ce1f89e461c2a26526857be0
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-08-28 19:24:21 +0800

    Update 199-mt798x.sh

--- patch/immortalwrt-24.10/199-mt798x.sh
+++ patch/immortalwrt-24.10/199-mt798x.sh
@@ -24,11 +24,8 @@ sed -i '$a src/gz openwrt_routing https://mirrors.pku.edu.cn/openwrt/releases/24
 sed -i '$a src/gz openwrt_telephony https://mirrors.pku.edu.cn/openwrt/releases/24.10.2/packages/aarch64_cortex-a53/telephony' /etc/opkg/distfeeds.conf
 sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_cortex-a53/kiddin9' /etc/opkg/customfeeds.conf
 
-#chmod 755 /etc/init.d/QINGYINSSIDMAC1.sh
 cp /etc/my-clash /etc/openclash/core/clash_meta
-#cp /etc/open-wifi.sh /root/open-wifi.sh
-chmod +x /root/open-wifi.sh
-opkg install /root/kwrt-luci-app-frpc.ipk
+#chmod +x /root/open-wifi.sh
 
 uci set wireless.default_radio1.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-5G
 uci set wireless.default_radio0.ssid=WiFi-$(cat /sys/class/ieee80211/phy0/macaddress|awk -F ":" '{print $5""$6 }' | tr 'a-z' 'A-Z')-2.4G
