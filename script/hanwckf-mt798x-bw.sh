6267562e7b65e36bf815872de24981214f658ac8
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-09-10 02:08:57 +0800

    Update hanwckf-mt798x-bw.sh

--- script/hanwckf-mt798x-bw.sh
+++ script/hanwckf-mt798x-bw.sh
@@ -57,9 +57,9 @@ mv package/small-package/luci-app-ikoolproxy package/luci-app-ikoolproxy
 rm -rf package/small-package
 
 # iStore
-git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
-git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
-git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+git clone --depth 1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth 1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth 1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
 mv package/nas-packages/network/services/* package/nas-packages/
 rm -rf package/nas-packages/network
 
