acd7ce0f687c244c6c35c64f72dfde479cead870
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-09-27 19:38:22 +0800

    Update mt798x-30wifi-closed.sh

--- patch/hanwckf/passwall/mt798x-30wifi-closed.sh
+++ patch/hanwckf/passwall/mt798x-30wifi-closed.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+uci commit
+
+sed -i '/ssrp/d' /etc/opkg/distfeeds.conf
+sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+cp /etc/my-clash /etc/openclash/core/clash_meta
+
+uci set wireless.MT7986_1_2.htmode='HE80'
+uci set wireless.MT7986_1_2.channel='44'
 # 路由器cpu芯片
 router_cpu=MT7986
 
