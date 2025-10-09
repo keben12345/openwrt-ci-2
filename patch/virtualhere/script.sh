444aa436781bccb658f25399e4fc4cbd83c3584e
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-10-09 11:01:45 +0800

    Update script.sh

--- patch/virtualhere/script.sh
+++ patch/virtualhere/script.sh
@@ -1,5 +1,5 @@
-sed -i 's/192.168.1.1/192.168.3.5/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.3.5/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.1.1/192.168.5.3/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.5.3/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 
