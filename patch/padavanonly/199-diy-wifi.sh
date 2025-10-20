286f2e7eb56265907aa3136b2c825837ca811d98
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-10-20 15:07:04 +0800

    Update 199-diy-wifi.sh

--- patch/padavanonly/199-diy-wifi.sh
+++ patch/padavanonly/199-diy-wifi.sh
@@ -8,6 +8,22 @@ uci set dropbear.@dropbear[0].Interface=''
 
 uci commit
 
+cat /diy4me/rules-pw2 >> /etc/config/passwall2
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+sed -i '/Modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '/filogic/d' /etc/opkg/distfeeds.conf
+echo > /etc/opkg/distfeeds.conf
+sed -i '$a src/gz openwrt_core https://mirrors.pku.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/mediatek/filogic/packages' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz openwrt_kmods https://mirrors.pku.edu.cn/immortalwrt/releases/24.10-SNAPSHOT/targets/mediatek/filogic/kmods/6.6.95-1-3ca4b8cb2fcc3a2027e8496143a86cab' /etc/opkg/distfeeds.conf
+echo > /etc/opkg/customfeeds.conf
+sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+cp /etc/my-clash /etc/openclash/core/clash_meta
+
 uci del network.wan6
 uci del network.lan.ip6assign
 uci del network.globals.ula_prefix
@@ -15,36 +31,62 @@ uci del dhcp.lan.dhcpv6
 uci del dhcp.lan.ndp
 uci del dhcp.lan.ra
 
-uci commit dhcp
-uci commit network
-
 #uci set wireless.default_MT7981_1_1.ssid=xiaoguo
-uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_1.key=12345678
-
+#uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_1.key=12345678
 #uci set wireless.default_MT7981_1_2.ssid=TK888
-uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_2.key=12345678
-uci commit wireless
-#uci commit
+#uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
+#uci set wireless.default_MT7981_1_2.key=12345678
+#uci commit wireless
 
-mymac=$(cat /sys/class/net/br-lan/address 2>/dev/null | awk -F: '{print $5 $6}' | tr 'a-z' 'A-Z')
-num=30
-wifipassword=12345678
-ipc=101
+uci set wireless.MT7986_1_2.htmode='HE80'
+uci set wireless.MT7986_1_2.channel='44'
+uci set wireless.default_MT7986_1_1.encryption=psk2+ccmp
+uci set wireless.default_MT7986_1_1.key=123456qwerty
+uci set wireless.default_MT7986_1_2.encryption=psk2+ccmp
+uci set wireless.default_MT7986_1_2.key=123456qwerty
+# 路由器cpu芯片
+router_cpu=MT7986
 
+# 新增WiFi数量
+increase_wifi_num=30
+
+# 除去默认，原本有几个WiFi
+existed_wifi_num=0
+
+# WiFi名称
+ssid=X60Pro
+
+# WiFi密码
+password=123456qwerty
+
+# WiFi地址
+ipaddr=10.10.1.1
+
+############################### 分 割 线 ###################################
+
+routermac=$(cat /sys/class/net/br-lan/address 2>/dev/null | awk -F: '{print $5 $6}' | tr 'a-z' 'A-Z')
+a=$(echo "$ipaddr" | awk -F. '{print $1}')
+b=$(echo "$ipaddr" | awk -F. '{print $2}')
+c=$(echo "$ipaddr" | awk -F. '{print $3}')
+d=$(echo "$ipaddr" | awk -F. '{print $4}')
+
+total_wifi=$((increase_wifi_num + existed_wifi_num))
 # 生成配置
-for i in $(seq 1 $num); do
+for i in $(seq $((existed_wifi_num + 1)) $total_wifi); do
     wifinet_num=$((i + 1))
-    new_c=$((ipc + i -1))
-    ipaddr="192.168.${new_c}.1"
+    new_c=$((c + i - 1))
+    new_ipaddr="${a}.${b}.${new_c}.${d}"
+
+    # 格式化接口编号为两位数
+    wifi_id=$(printf "%02d" $i)
 
     # 根据序号选择wireless设备
     if [ $i -le 15 ]; then
-        wireless_dev="MT7981_1_2"
+        wireless_dev="${router_cpu}_1_2"
         network_dev="rax${i}"
     else
-        wireless_dev="MT7981_1_1"
+        wireless_dev="${router_cpu}_1_1"
         network_dev="ra$((i - 15))"
     fi
 
@@ -52,40 +94,34 @@ for i in $(seq 1 $num); do
     uci set wireless.wifinet${wifinet_num}=wifi-iface
     uci set wireless.wifinet${wifinet_num}.device="$wireless_dev"
     uci set wireless.wifinet${wifinet_num}.mode='ap'
-    uci set wireless.wifinet${wifinet_num}.ssid="wifi-${i}"
+    uci set wireless.wifinet${wifinet_num}.ssid="${ssid}-${wifi_id}"
     uci set wireless.wifinet${wifinet_num}.encryption='psk2+ccmp'
-    uci set wireless.wifinet${wifinet_num}.key="$wifipassword"
-    # uci set wireless.wifinet${wifinet_num}.ifname="ap${i}"
-    uci set wireless.wifinet${wifinet_num}.network="wifi${i}"
+    uci set wireless.wifinet${wifinet_num}.key="$password"
+    uci set wireless.wifinet${wifinet_num}.network="wifi${wifi_id}"
 
     # 配置网络接口
-    uci set network.wifi${i}=interface
-    uci set network.wifi${i}.proto='static'
-    uci set network.wifi${i}.device="${network_dev}"
-    uci set network.wifi${i}.ipaddr="$ipaddr"
-    uci set network.wifi${i}.netmask='255.255.255.0'
+    uci set network.wifi${wifi_id}=interface
+    uci set network.wifi${wifi_id}.proto='static'
+    uci set network.wifi${wifi_id}.device="${network_dev}"
+    uci set network.wifi${wifi_id}.ipaddr="$new_ipaddr"
+    uci set network.wifi${wifi_id}.netmask='255.255.255.0'
 
     # 配置DHCP
-    uci set dhcp.wifi${i}=dhcp
-    uci set dhcp.wifi${i}.interface="wifi${i}"
+    uci set dhcp.wifi${wifi_id}=dhcp
+    uci set dhcp.wifi${wifi_id}.interface="wifi${wifi_id}"
 
     # 配置防火墙
     uci add firewall zone
-    uci set firewall.@zone[-1].name="wifi${i}"
+    uci set firewall.@zone[-1].name="wifi${wifi_id}"
     uci set firewall.@zone[-1].input='ACCEPT'
     uci set firewall.@zone[-1].output='ACCEPT'
     uci set firewall.@zone[-1].forward='ACCEPT'
-    uci add_list firewall.@zone[-1].network="wifi${i}"
+    uci add_list firewall.@zone[-1].network="wifi${wifi_id}"
+
     uci add firewall forwarding
-    uci set firewall.@forwarding[-1].src="wifi${i}"
+    uci set firewall.@forwarding[-1].src="wifi${wifi_id}"
     uci set firewall.@forwarding[-1].dest='wan'
-    
-    uci add firewall rule
-    uci set firewall.@rule[-1].src="wifi${i}"
-    uci set firewall.@rule[-1].dest='wan'
-    uci set firewall.@rule[-1].name="wifi${i}"
-    uci add_list firewall.@rule[-1].proto='all'
-    uci set firewall.@rule[-1].target='REJECT'
+
 done
 
 # 提交配置
@@ -94,15 +130,12 @@ uci commit network
 uci commit dhcp
 uci commit firewall
 
-
-sed -i 's/root::0:0:99999:7:::/root:$1$iOzd6YV5$fFw/2Cd.2prhGTiuCPCj0.:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/root:::0:99999:7:::/root:$1$iOzd6YV5$fFw/2Cd.2prhGTiuCPCj0.:0:0:99999:7:::/g' /etc/shadow
-
-cp /etc/my-clash /etc/openclash/core/clash_meta
-
+# 重启服务
+echo -e "\n配置已完成！"
 /etc/init.d/network restart >/dev/null 2>&1
 /etc/init.d/firewall restart >/dev/null 2>&1
 /etc/init.d/dnsmasq restart >/dev/null 2>&1
 /etc/init.d/dropbear restart >/dev/null 2>&1
+
 
 exit 0
